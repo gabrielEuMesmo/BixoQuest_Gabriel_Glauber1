458d2f22b4c46c593fd322d8f32cc327e56d05f7
gabri <gabriel4141.a@gmail.com>
2026-04-09 20:55:48 -0300

    Menu base

--- src/model/Menu.java
+++ src/model/Menu.java
@@ -23,4 +23,27 @@ public abstract class Menu {
 
     }
 
+    public int getSaldoSaude(){return saldoSaude; }
+
+    public int getSaldoMotivacao(){return saldoMotivacao; }
+
+    public int getSaldoDes_acad_m1(){return saldoDes_acad_m1; }
+
+    public int getSaldoDes_acad_m2(){return saldoDes_acad_m2; }
+
+    public int getSaldoDes_acad_m3(){return saldoDes_acad_m3; }
+
+    public double getSaldoDinheiro(){return saldoDinheiro; }
+
+    public void setSaldoSaude(int valor){saldoSaude = valor; }
+
+    public void setSaldoMotivacao(int valor){saldoMotivacao = valor; }
+
+    public void setSaldoDinheiro(double valor){saldoDinheiro = valor; }
+
+    public void setSaldoDes_acad_m1(int valor){saldoDes_acad_m1= valor; }
+
+    public void setSaldoDes_acad_m2(int valor){saldoDes_acad_m2= valor; }
+
+    public void setSaldoDes_acad_m3(int valor){saldoDes_acad_m3= valor; }
 }
